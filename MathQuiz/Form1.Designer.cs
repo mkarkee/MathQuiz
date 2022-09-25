0ca23460d00f50c38abce050e6bade428530e6c7
mkarkee <ermkarki@gmail.com>
2022-09-24 19:28:19 -0600

    timeLeft

--- MathQuiz/Form1.Designer.cs
+++ MathQuiz/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace MathQuiz
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.timeLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.plusLeftLabel = new System.Windows.Forms.Label();
@@ -52,6 +53,8 @@ namespace MathQuiz
             this.dividedRightLabel = new System.Windows.Forms.Label();
             this.dividedLeftLabel = new System.Windows.Forms.Label();
             this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dateLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.sum)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.difference)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.product)).BeginInit();
@@ -61,7 +64,7 @@ namespace MathQuiz
             // timeLabel
             // 
             this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.timeLabel.Location = new System.Drawing.Point(272, 20);
             this.timeLabel.Name = "timeLabel";
             this.timeLabel.Size = new System.Drawing.Size(200, 30);
@@ -70,10 +73,10 @@ namespace MathQuiz
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(165, 21);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(179, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 25);
+            this.label1.Size = new System.Drawing.Size(87, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "Time Left";
             // 
@@ -124,6 +127,8 @@ namespace MathQuiz
             this.sum.Name = "sum";
             this.sum.Size = new System.Drawing.Size(100, 35);
             this.sum.TabIndex = 1;
+            this.sum.ValueChanged += new System.EventHandler(this.check_answer);
+            this.sum.Click += new System.EventHandler(this.answer_Enter);
             // 
             // difference
             // 
@@ -132,6 +137,9 @@ namespace MathQuiz
             this.difference.Name = "difference";
             this.difference.Size = new System.Drawing.Size(100, 35);
             this.difference.TabIndex = 2;
+            this.difference.ValueChanged += new System.EventHandler(this.check_answer);
+            this.difference.Click += new System.EventHandler(this.answer_Enter);
+            this.difference.Enter += new System.EventHandler(this.answer_Enter);
             // 
             // label2
             // 
@@ -180,6 +188,8 @@ namespace MathQuiz
             this.product.Name = "product";
             this.product.Size = new System.Drawing.Size(100, 35);
             this.product.TabIndex = 3;
+            this.product.ValueChanged += new System.EventHandler(this.check_answer);
+            this.product.Click += new System.EventHandler(this.answer_Enter);
             // 
             // label8
             // 
@@ -228,6 +238,8 @@ namespace MathQuiz
             this.quotient.Name = "quotient";
             this.quotient.Size = new System.Drawing.Size(100, 35);
             this.quotient.TabIndex = 4;
+            this.quotient.ValueChanged += new System.EventHandler(this.check_answer);
+            this.quotient.Click += new System.EventHandler(this.answer_Enter);
             // 
             // label12
             // 
@@ -281,11 +293,26 @@ namespace MathQuiz
             this.startButton.UseVisualStyleBackColor = true;
             this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.Location = new System.Drawing.Point(12, 21);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(0, 24);
+            this.dateLabel.TabIndex = 21;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.dateLabel);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.quotient);
             this.Controls.Add(this.label12);
@@ -311,7 +338,8 @@ namespace MathQuiz
             this.Controls.Add(this.timeLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
-            this.Text = "Math Quiz";
+            this.Text = "Milan Karki Math Quiz";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.sum)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.difference)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.product)).EndInit();
@@ -346,6 +374,8 @@ namespace MathQuiz
         private System.Windows.Forms.Label dividedRightLabel;
         private System.Windows.Forms.Label dividedLeftLabel;
         private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label dateLabel;
     }
 }
 
--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -31,6 +31,10 @@ namespace MathQuiz
         int dividend;
         int divisor;
 
+        //variable to keep track of remaining time
+        int timeLeft;
+
+
 
 
         //Start the quiz by filling up problems and 
@@ -67,19 +71,136 @@ namespace MathQuiz
             dividedRightLabel.Text = divisor.ToString();
             quotient.Value = 0;
 
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
 
 
+        }
+
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtraend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else
+                return false;
         }
 
         public Form1()
         {
             InitializeComponent();
+
+            DateTime thisDay = DateTime.Today;
+            String toDate = String.Format("{0:d MMMM yyyy}", thisDay);
+
+            dateLabel.Text = toDate;
         }
 
         private void startButton_Click(object sender, EventArgs e)
         {
             StartTheQuiz();
             startButton.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(CheckTheAnswer())
+            {
+                //if all the answers are correct
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!",
+                                   "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if(timeLeft > 0) //if the answers arent correct
+            {
+                timeLeft -= 1;
+                timeLabel.Text = timeLeft + "seconds";
+                if(timeLeft < 6)
+                {
+                    timeLabel.BackColor = Color.Red;
+                }
+            }
+   
+            else //if runs out of time
+            {
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtraend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+                timeLabel.BackColor = DefaultBackColor;
+            }
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+        }
+
+        private void check_answer(object sender, EventArgs e)
+        {
+            NumericUpDown answerBox = sender as NumericUpDown;
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\Windows\Media\notify.wav");
+
+            if (answerBox.Name == "sum")
+            {
+                
+                if (answerBox.Value == addend1 + addend2)
+                {
+                    player.Play();
+                }
+
+            }
+
+            if (answerBox.Name == "difference")
+            {
+
+                if (answerBox.Value == minuend - subtraend)
+                {
+                    player.Play();
+                }
+
+            }
+
+            if (answerBox.Name == "product")
+            {
+
+                if (answerBox.Value == multiplicand * multiplier)
+                {
+                    player.Play();
+                }
+
+            }
+
+            if (answerBox.Name == "quotient")
+            {
+
+                if (answerBox.Value == dividend / divisor)
+                {
+                    player.Play();
+                }
+
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
